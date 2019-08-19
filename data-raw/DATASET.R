5eee2605b9fd10ada5ba97c1159e9fc22f69ee94
Michael Sumner <mdsumner@gmail.com>
2019-08-19 05:39:16 +0000

    example raster data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,12 @@
 ## code to prepare `DATASET` dataset goes here
 minimal_mesh <- silicate::minimal_mesh
-save(minimal_mesh, file = "data-raw/geo-comp-graphics.Rdata")
+
+loc <- cbind(7 + 37.5/60, 51 + 57.6/60)
+buffer <- 9000
+library(ceramic)
+
+#munster_elev <- cc_elevation(loc, buffer, zoom = 12)
+#munster_imag <- cc_location(loc, buffer, zoom = 14)
+#munster_qmesh <- quadmesh::quadmesh(munster_elev, texture = munster_imag, texture_filename = "data-raw/file160c6c1729b3.png")
+munster_elev <- readRDS("data-raw/munster_elev.rds")
+save(minimal_mesh, munster_elev, file = "data-raw/geo-comp-graphics.Rdata")
